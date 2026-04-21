4aae8d94a168ac4318f83ae4c61c53605f9bd488
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-04-21 19:58:23 +0200

    feat : CORS

--- api/riders/index.js
+++ api/riders/index.js
@@ -1,4 +1,12 @@
 export default function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const riders = [
     { id: "1", name: "Julien" },
     { id: "2", name: "Sarah" }
