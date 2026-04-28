cffdb5c3b50ccb95fc3adb1dba6ef5794a17ff6d
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-04-28 11:14:34 +0200

    feat : ajout des CORS

--- pages/api/classifications/index.js
+++ pages/api/classifications/index.js
@@ -2,6 +2,14 @@ import classifications from '../../../data/classifications.json'
 import riders from '../../../data/riders.json'
 
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+  
   const result = {}
 
   for (const key in classifications) {
--- pages/api/riders/[id].js
+++ pages/api/riders/[id].js
@@ -1,6 +1,14 @@
 import riders from '../../../data/riders.json'
 
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+  
   const { id } = req.query
 
   const rider = riders.find(r => r.id === parseInt(id))
